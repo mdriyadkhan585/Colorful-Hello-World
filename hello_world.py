aa799a2bb9658cf6160b946ccd9c9594392ce1b7
RK Studio <107482047+mdriyadkhan585@users.noreply.github.com>
2024-08-31 22:31:27 +0600

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+from colorama import Fore, Style, init
+
+# Initialize colorama
+init()
+
+# Print a colorful "Hello, World!" message
+print(Fore.RED + Style.BRIGHT + "Hello, World!" + Style.RESET_ALL)
+print(Fore.GREEN + "Hello, World!" + Style.RESET_ALL)
+print(Fore.BLUE + "Hello, World!" + Style.RESET_ALL)
